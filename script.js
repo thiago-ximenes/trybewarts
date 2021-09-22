45b8f2b698dae72b653156b12365d45d29779a52
Sergio <sergio.n.m.jr@gmail.com>
2021-09-22 16:42:15 -0300

    ate o 3

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const loginButton = document.querySelector("#login");
-const emailText = document.querySelector("#email").value;
-const senha = document.querySelector("#senha").value;
 
 
 login.addEventListener("click", () => {
+    const emailText = document.querySelector("#email").value;
+    const senha = document.querySelector("#senha").value;
+    
     if(emailText === 'tryber@teste.com' && senha === "123456"){
         alert('Olá, Tryber!!');
+        alert(emailText + senha);
     }
     else{
         alert('Email ou senha inválidos.')
